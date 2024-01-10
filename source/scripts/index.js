55aed11cad00da58981c65293a322eee12bc0885
Denis Nikonov <nikonovdenis1991@yandex.ru>
2024-01-10 21:01:59 +0300

    добавлена рабочая кнопка меню мобильной версии индекса

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.menu-nav');
+let navOpen = document.querySelector('.header__toggle-open');
+let navClose = document.querySelector('.menu-nav__toggle-close');
+
+navMain.classList.remove('menu-nav--no-js');
+
+navOpen.addEventListener('click', function () {
+  if (navMain.classList.contains('menu-nav--closed')) {
+    navMain.classList.add('menu-nav--opened');
+    navMain.classList.remove('menu-nav--closed');
+  }
+});
+
+navClose.addEventListener('click', function () {
+  if (navMain.classList.contains('menu-nav--opened')) {
+    navMain.classList.add('menu-nav--closed');
+    navMain.classList.remove('menu-nav--opened');
+  }
+});
