f43051239736e1bacfdaff98c23ce0e71349ef44
Denis Nikonov <nikonovdenis1991@yandex.ru>
2024-01-10 21:08:31 +0300

    добавлена рабочая кнопка меню мобильной версии и пройден линтер на скрипты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,18 +1,18 @@
 /* в этот файл добавляет скрипты*/
-let navMain = document.querySelector('.menu-nav');
-let navOpen = document.querySelector('.header__toggle-open');
-let navClose = document.querySelector('.menu-nav__toggle-close');
+const navMain = document.querySelector('.menu-nav');
+const navOpen = document.querySelector('.header__toggle-open');
+const navClose = document.querySelector('.menu-nav__toggle-close');
 
 navMain.classList.remove('menu-nav--no-js');
 
-navOpen.addEventListener('click', function () {
+navOpen.addEventListener('click', () => {
   if (navMain.classList.contains('menu-nav--closed')) {
     navMain.classList.add('menu-nav--opened');
     navMain.classList.remove('menu-nav--closed');
   }
 });
 
-navClose.addEventListener('click', function () {
+navClose.addEventListener('click', () => {
   if (navMain.classList.contains('menu-nav--opened')) {
     navMain.classList.add('menu-nav--closed');
     navMain.classList.remove('menu-nav--opened');
