cb0044d42d842c92238febcedd35125281efb1ae
Denis Nikonov <nikonovdenis1991@yandex.ru>
2024-01-14 17:18:48 +0300

    добавлен и реализован скрипт кнопки навигации в мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.menu-nav');
+const navOpen = document.querySelector('.header__toggle-open');
+const navClose = document.querySelector('.menu-nav__toggle-close');
+
+navMain.classList.remove('menu-nav--no-js');
+
+navOpen.addEventListener('click', () => {
+  if (navMain.classList.contains('menu-nav--closed')) {
+    navMain.classList.add('menu-nav--opened');
+    navMain.classList.remove('menu-nav--closed');
+  }
+});
+
+navClose.addEventListener('click', () => {
+  if (navMain.classList.contains('menu-nav--opened')) {
+    navMain.classList.add('menu-nav--closed');
+    navMain.classList.remove('menu-nav--opened');
+  }
+});
